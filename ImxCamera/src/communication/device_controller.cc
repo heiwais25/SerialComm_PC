e9d3e4700c807e4887b8d657976cdeba81fb2fa2
jonghyun <heiwais25@gmail.com>
2018-02-09 20:53:27 +0900

    Add png compression function of cut off image

--- ImxCamera/src/communication/device_controller.cc
+++ ImxCamera/src/communication/device_controller.cc
@@ -109,17 +109,14 @@ void DeviceController::DoImageTransmission(void) {
 	switch (c)
 	{
 		case '1':
-			//pImageProcessing_->SetImageType(RAW_IMAGE_FORMAT);
 			SendCommand(CAMERA_SEND_CAPTURED_IMAGE);
 			break;
 
 		case '2':
-			//pImageProcessing_->SetImageType(PACKED_RAW_IMAGE_FORMAT);
 			SendCommand(CAMERA_SEND_PACKED_DATA);
 			break;
 
 		case '3':
-			//pImageProcessing_->SetImageType(PACKED_PNG_IMAGE_FORMAT);
 			SendCommand(CAMERA_SEND_PNG);
 			break;
 
@@ -128,6 +125,10 @@ void DeviceController::DoImageTransmission(void) {
 			break;
 
 		case '5':
+			SendCommand(CAMERA_SEND_PACKED_PNG_CUT_OFF_IMAGE);
+			break;
+
+		case '6':
 			SendCommand(COMMAND_CAMERA_SEND_LAST_ACTIVATED_MODE);
 			break;
 
@@ -175,18 +176,22 @@ void DeviceController::DoImageProcess(void) {
 			break;
 
 		case '4':
-			SendCommand(SAVE_IMAGE);
+			SendCommand(PACK_CUT_OFF_CONVERT_PNG);
 			break;
 
 		case '5':
-			SetImageToRead();
+			SendCommand(SAVE_IMAGE);
 			break;
 
 		case '6':
-			SendCommand(SHOW_STORED_IMAGE_LIST);
+			SetImageToRead();
 			break;
 
 		case '7':
+			SendCommand(SHOW_STORED_IMAGE_LIST);
+			break;
+
+		case '8':
 			SendCommand(DELETE_LAST_NODE);
 			break;
 
@@ -613,7 +618,8 @@ void DeviceController::ShowImageTransmissionOption(void) {
 	std::cout << "2) Send packed image format" << std::endl;
 	std::cout << "3) Send packed png format" << std::endl;
 	std::cout << "4) Send cut off image" << std::endl;
-	std::cout << "5) Send last activated mode" << std::endl;
+	std::cout << "4) Send packed cut off png image" << std::endl;
+	std::cout << "6) Send last activated mode" << std::endl;
 	std::cout << "x) Go to previous menu" << std::endl;
 }
 
@@ -622,10 +628,11 @@ void DeviceController::ShowImageProcessOption(void) {
 	std::cout << "1) Pack raw image data" << std::endl;
 	std::cout << "2) Convert packed image to png " << std::endl;
 	std::cout << "3) Cut image edges" << std::endl;
-	std::cout << "4) Save image data to emmc" << std::endl;
-	std::cout << "5) Read image data from emmc" << std::endl;
-	std::cout << "6) Show stored image list" << std::endl;
-	std::cout << "7) Delete last node" << std::endl;
+	std::cout << "4) Convert packed cut off image edges to png" << std::endl;
+	std::cout << "5) Save image data to emmc" << std::endl;
+	std::cout << "6) Read image data from emmc" << std::endl;
+	std::cout << "7) Show stored image list" << std::endl;
+	std::cout << "8) Delete last node" << std::endl;
 	std::cout << "x) Go to previous menu" << std::endl;
 }
 
